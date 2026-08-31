1b477653a115c9a4bee24d7bee941771e6a6dee2
Fund It Away <quoteitaway@gmail.com>
2026-08-31 17:41:24 -0500

    Expose in-house handoff and agent draft on the engine

--- api/_engine.js
+++ api/_engine.js
@@ -1,1 +1,5 @@
-module.exports = require("./engine");
+const engine = require("./engine");
+const hand = require("./_handoff");
+module.exports = Object.assign({}, engine, hand, {
+  crewOf: hand.crewOf || engine.crewOf
+});
