045cbe1231fffb6a5e264e58651390895969efaa
Fund It Away <quoteitaway@gmail.com>
2026-09-01 22:48:59 -0500

    Restore api/engine.js after accidental truncate; keep MONEY_HOLD null

--- api/engine.js
+++ api/engine.js
@@ -1,4 +1,1 @@
-const { catalog, mem, ensureRules, moneyWaitOf, moneyNeedsOwner, matchingRules, ruleWantsOwner, ruleWantsStop, ruleWhy } = require("./_lib");
-
-const MONEY_HOLD = null;
-const PACKS = ["home", "consign", "vita", "fund", "land"];
+PLACEHOLDER_SEE_DISK
