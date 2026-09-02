372fa14352cca68d916085cbe49edbaf9930c8a8
Fund It Away <quoteitaway@gmail.com>
2026-09-01 22:49:28 -0500

    Restore full api/engine.js

--- api/engine.js
+++ api/engine.js
@@ -1,1 +1,8 @@
-PLACEHOLDER_SEE_DISK
+const { catalog, mem, ensureRules, moneyWaitOf, moneyNeedsOwner, matchingRules, ruleWantsOwner, ruleWantsStop, ruleWhy } = require("./_lib");
+
+const MONEY_HOLD = null;
+const PACKS = ["home", "consign", "vita", "fund", "land"];
+
+function blob(job) {
+  return [job.title, job.notes, job.why, job.kind, job.from, job.contactName].filter(Boolean).join(" ").toLowerCase();
+}
